5c6eafab953060910226a10c2f7df269014e3ebd
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-10-03 16:36:14 +0200

    Cleaned up main app interface.

--- ak-tagger/source/app.d
+++ ak-tagger/source/app.d
@@ -10,24 +10,7 @@ import asdf;
 import commandr;
 
 void main(string[] args) {
-	auto prog = new Program("ak-tagger")
-		.summary("Architectural knowledge categorization program.")
-		.add(new Command("help"))
-		.add(new Command("fetch"))
-		.add(new Command("use")
-			.add(new Argument("file", "The file to use.")))
-		.add(new Command("export")
-			.add(new Option("f", "file", "The file to export to.")))
-		.add(new Command("import")
-			.add(new Option("f", "file", "The file to import from.").acceptsFiles()))
-		.defaultCommand("help");
-
-	auto pArgs = prog.parse(args);
-
-	pArgs
-		.on("help", (ProgramArgs args) {
-			prog.printHelp();
-		})
+	parseArgs(args)
 		.on("fetch", (args) => fetch(args))
 		.on("use", (args) => use(args));
 }
@@ -46,4 +29,20 @@ void fetch(ProgramArgs args) {
 void use(ProgramArgs args) {
 	auto filename = args.arg("file");
 	useData(filename);
+}
+
+private ProgramArgs parseArgs(string[] args) {
+	auto prog = new Program("ak-tagger")
+		.summary("Architectural knowledge categorization program.")
+		.add(new Command("help"))
+		.add(new Command("fetch"))
+		.add(new Command("use")
+			.add(new Argument("file", "The file to use.")))
+		.defaultCommand("help");
+
+	auto pArgs = prog.parse(args);
+	pArgs.on("help", (ProgramArgs args) {
+		prog.printHelp();
+	});
+	return pArgs;
 }
